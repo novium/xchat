5105d2386d04ed245efb160cc356af6bbb6cd1fc
novium <mail@novium.pw>
2018-11-30 11:25:15 +0100

    Updated test, refactored getip

--- tests/dns.test.js
+++ tests/dns.test.js
@@ -5,14 +5,8 @@ import ip from '../src/lib/getip';
 import { PerformanceObserver, performance } from 'perf_hooks';
 
 test('get ip', async t => {
-    let avg = [];
-    for(let i = 0; i < 5; i++) {
-        const t0 = performance.now();
-        const res = await ip.getIP();
-        const t1 = performance.now();
-        avg[i] = (t1 - t0);
-    }
+    const res = await ip.getIP();
 
-    console.log("avg: " + avg.reduce((acc, b) => { return acc + b; }) / avg.length);
-    t.pass();
+    // Can't do much more than this.
+    t.truthy(res.length != 0);
 });
