0cb0ad91506adb6251c56d5e7a9c595e9b4d5ff7
borgen1 <isak.borg.ib@gmail.com>
2018-12-04 11:01:33 +0100

    Added some framework. Was going to start implementing, but ECONNREFUSED

--- src/dht/dht.js
+++ src/dht/dht.js
@@ -1,19 +1,41 @@
 import DHT from 'bittorrent-dht';
-
-export default class {
+ export default class {
     _dht;
 
-    constructor() {
+     constructor() { //Easier to just use let ourDHT = new DHT() but oh well.
         this._dht = new DHT();
-
-        this._dht.on('peer', this.onPeer);
+        this._dht.listen();
     }
 
-    onPeer(peer, infoHash, from) {
+    this._dht.on('peer', function (peer, infoHash, from) {
+      console.log('found potential peer ' + peer.host + ':' + peer.port + ' through ' + from.address + ':' + from.port)
+    });
 
+    this._dht.on('listening', function{
+      console.log("DHT is now listening");
+    });
+
+    this._dht.on('error', function(err){
+      console.log("Error experienced");
+      throw(err);
+    });
+
+    this._dht.on('node', function(newNode){
+        this._dht.addNode(newNode);
+    });
+
+    this._dht.on('announce', function(peer, infoHash){
+
+    });
+
+    infoGet(hash, callback){
+      this._dht.get(hash, NULL, callback);
+    }
+
+    endDHT() {
+      this._dht.destroy();
     }
 
     announce(room) {
-
     }
 }
