d1e31baee58559a43cdf11b3915052e85e0c80f1
Borgen1 <isak.borg.ib@gmail.com>
2018-12-07 10:53:17 +0100

    fixed minor errors

--- src/start.js
+++ src/start.js
@@ -3,4 +3,4 @@ require('@babel/register')({
     plugins: [ "@babel/plugin-transform-flow-strip-types", "@babel/plugin-proposal-class-properties" ]
 });
 
-module.exports = require('./mains.js');
+module.exports = require('./main.js');
